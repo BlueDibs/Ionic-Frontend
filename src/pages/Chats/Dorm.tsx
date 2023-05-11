97f6939055ae0de2f572f86ef2156681ebb9594f
amiralam0 <sharpprogrammer2018@gmail.com>
2023-05-12 00:12:17 +0530

    room remove by swipe dorm

--- src/pages/Chats/Dorm.tsx
+++ src/pages/Chats/Dorm.tsx
@@ -1,12 +1,21 @@
-import { IonIcon, IonItem, IonLabel, IonPage } from '@ionic/react';
+import {
+  IonIcon,
+  IonItem,
+  IonItemOption,
+  IonItemOptions,
+  IonItemSliding,
+  IonLabel,
+  IonPage,
+} from '@ionic/react';
 import { useAppSelector } from '../../store/hooks';
 import { MutableRefObject, useEffect, useRef, useState } from 'react';
-import { get, onValue, ref } from 'firebase/database';
+import { get, onValue, ref, remove } from 'firebase/database';
 import { database } from '../../utils/firebase';
 import { ActionIcon, Avatar, Flex, Paper } from '@mantine/core';
 import { useQuery } from '@tanstack/react-query';
 import { getProfiles } from './api.chat';
 import { chatbubbleEllipsesOutline } from 'ionicons/icons';
+import { Dialog } from '@capacitor/dialog';
 
 import './style.css';
 import { useHistory } from 'react-router';
@@ -38,20 +47,44 @@ export function Dorm() {
     return unsub;
   }, [dormStoreRefURI]);
 
+  const deleteDorm = async (targetUserId: string) => {
+    const itemSwiper = document.getElementById(`ionic-swiper-${targetUserId}`);
+    if (itemSwiper) itemSwiper?.close();
+    const { value } = await Dialog.confirm({
+      title: 'Confirm',
+      message: `Confirm delete chat?`,
+    });
+    if (value) remove(ref(database, `${dormStoreRefURI}/${targetUserId}`));
+  };
+
   return (
     <IonPage style={{ display: 'block' }}>
       {getProfilesQuery.data?.map(
         (profile: { username: string; avatarPath: string; id: string }) => (
-          <IonItem
-            href="javascript:void"
-            onClick={() => history.push(`/app/chat/${profile.id}`)}
-            lines="full"
-          >
-            <Avatar />
-            <IonLabel style={{ marginLeft: '10px' }}>
-              {profile.username}
-            </IonLabel>
-          </IonItem>
+          <IonItemSliding id={`ionic-swiper-${profile.id}`}>
+            <IonItem
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                history.push(`/app/chat/${profile.id}`);
+              }}
+              lines="full"
+            >
+              <Avatar />
+              <IonLabel style={{ marginLeft: '10px' }}>
+                {profile.username}
+              </IonLabel>
+            </IonItem>
+
+            <IonItemOptions>
+              <IonItemOption
+                onClick={() => deleteDorm(profile.id)}
+                color="danger"
+              >
+                Delete
+              </IonItemOption>
+            </IonItemOptions>
+          </IonItemSliding>
         )
       )}
     </IonPage>
