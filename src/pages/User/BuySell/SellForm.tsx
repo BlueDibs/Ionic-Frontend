5a7b3cf1c42cc37c9e5773c79cbd51ca11f2af1f
amiralam0 <sharpprogrammer2018@gmail.com>
2023-06-02 00:37:58 +0530

    buy sell form

--- src/pages/User/BuySell/SellForm.tsx
+++ src/pages/User/BuySell/SellForm.tsx
@@ -1,6 +1,14 @@
 import { Flex, TextInput, Button, Text } from '@mantine/core';
+import { useForm } from '@mantine/form';
+import { useMutation } from '@tanstack/react-query';
+import { sellSharesAPI } from './buySell.api';
 
 export function SellForm({ userData }: { userData: any }) {
+  const sellForm = useForm();
+  const sell_shares_mut = useMutation({
+    mutationFn: (vals: any) => sellSharesAPI(userData.id, vals),
+  });
+
   return (
     <>
       <Flex direction={'column'} gap={'md'} p={'lg'}>
@@ -18,26 +26,26 @@ export function SellForm({ userData }: { userData: any }) {
           value={`${userData.shares}`}
         />
 
-        {/* <TextInput
+        <TextInput
           variant="filled"
           label="Total"
           style={{ pointerEvents: 'none' }}
-          {...form.getInputProps('amount')}
-          value={`₹ ${(form.values.amount || 0) * userData.price}`}
+          {...sellForm.getInputProps('total')}
+          value={`₹ ${(sellForm.values.amount || 0) * userData.price}`}
         />
 
         <TextInput
           type="number"
           variant="filled"
           label="Amount"
-          {...form.getInputProps('total')}
+          {...sellForm.getInputProps('amount')}
           onChange={(e) => {
-            form.setValues({
+            sellForm.setValues({
               amount: parseInt(e.target.value),
               total: parseInt(e.target.value) * userData.price,
             });
           }}
-        /> */}
+        />
 
         <Flex gap={'xs'}>
           <Text size={'sm'} weight={500}>
@@ -55,6 +63,7 @@ export function SellForm({ userData }: { userData: any }) {
 
       <Button
         size="md"
+        onClick={() => sell_shares_mut.mutate(sellForm.values)}
         variant="filled"
         color="red"
         style={{
