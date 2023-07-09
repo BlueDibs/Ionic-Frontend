28463eb1ed440ffce55e7df90aa7300cd621549d
MohdAhmad1 <dexk2015@gmail.com>
2023-07-09 18:55:19 +0530

    Update Search.tsx

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -7,20 +7,22 @@ import {
 } from "@ionic/react";
 import { useQuery } from "@tanstack/react-query";
 import { searchUsername } from "./search.api";
-import { useEffect, useRef, useState } from "react";
-import { Avatar, Loader } from "@mantine/core";
+import { useEffect, useState } from "react";
+import { Avatar, Loader, Popover, ScrollArea, Text } from "@mantine/core";
 import { config } from "../../config";
 import { queryClient } from "../../utils/queryClient";
 import { useHistory } from "react-router";
-import { useDebouncedState } from "@mantine/hooks";
+import { useDebouncedState, useDebouncedValue } from "@mantine/hooks";
 
 export function Search() {
   const history = useHistory();
   const [search, setSearchQuery] = useState("");
+  const [isPopoverOpened, setPopoverOpened] = useState(false);
+  const [debounced] = useDebouncedValue(search, 200);
 
   const searchQuery = useQuery({
     queryKey: ["search"],
-    queryFn: () => searchUsername(search ?? ""),
+    queryFn: () => searchUsername(debounced ?? ""),
     refetchOnWindowFocus: false,
     enabled: false,
     placeholderData: [],
@@ -30,47 +32,77 @@ export function Search() {
     if (search?.length) {
       searchQuery.refetch();
     } else {
-      queryClient.setQueryData(["search"], []);
+      queryClient.setQueryData(["search"], () => []);
     }
-  }, [search]);
+  }, [debounced]);
 
   useEffect(() => {
     if (queryClient.getQueriesData(["search"])) {
-      queryClient.setQueryData(["search"], []);
+      queryClient.setQueryData(["search"], () => []);
     }
+
+    setPopoverOpened(true);
   }, [history.location]);
 
   return (
     <IonPage style={{ display: "block" }}>
-      <IonSearchbar
-        debounce={200}
-        onIonClear={() => queryClient.setQueryData(["search"], [])}
-        onIonChange={(ev) => {
-          searchQuery.refetch();
-        }}
-        animated={true}
-        onIonInput={(event) => setSearchQuery(event.target.value ?? "")}
-        placeholder="username"
-      ></IonSearchbar>
+      <Popover
+        withinPortal
+        withArrow
+        styles={{ dropdown: { width: "90vw !important" } }}
+        opened={isPopoverOpened && !!search.length}
+        onClose={() => setPopoverOpened(false)}
+      >
+        <Popover.Target>
+          <IonSearchbar
+            animated={true}
+            onIonInput={(event) => {
+              setSearchQuery(event.target.value ?? "");
+              event.target.value && setPopoverOpened(true);
+            }}
+            value={search}
+            placeholder="username"
+            onIonFocus={() => setPopoverOpened(true)}
+            onIonBlur={() => setPopoverOpened(false)}
+          ></IonSearchbar>
+        </Popover.Target>
 
-      {searchQuery.isFetched && <Loader my="md" />}
+        <Popover.Dropdown>
+          {searchQuery.isFetching && <Loader m={10} />}
 
-      <IonList>
-        {Array.isArray(searchQuery.data) &&
-          searchQuery.data.map((user) => (
-            <IonItem onClick={() => history.push(`/app/user/${user.id}`)}>
-              <Avatar
-                style={{ marginRight: 10, borderRadius: "50%" }}
-                src={
-                  user.avatarPath
-                    ? `${config.STATIC_FILE_BASE_URL}${user.avatarPath}?alt=media`
-                    : null
-                }
-              />
-              <IonLabel>{user.username}</IonLabel>
-            </IonItem>
-          ))}
-      </IonList>
+          {!searchQuery.data.length && !searchQuery.isFetching && (
+            <Text size="sm" color="dimmed">
+              No results found
+            </Text>
+          )}
+
+          <ScrollArea.Autosize type="always" style={{ maxHeight: "30vh" }}>
+            <IonList>
+              {Array.isArray(searchQuery.data) &&
+                searchQuery.data.map((user) => (
+                  <IonItem
+                    onClick={() => {
+                      setPopoverOpened(false);
+                      setSearchQuery("");
+
+                      history.push(`/app/user/${user.id}`);
+                    }}
+                  >
+                    <Avatar
+                      style={{ marginRight: 10, borderRadius: "50%" }}
+                      src={
+                        user.avatarPath
+                          ? `${config.STATIC_FILE_BASE_URL}${user.avatarPath}?alt=media`
+                          : null
+                      }
+                    />
+                    <IonLabel>{user.username}</IonLabel>
+                  </IonItem>
+                ))}
+            </IonList>
+          </ScrollArea.Autosize>
+        </Popover.Dropdown>
+      </Popover>
     </IonPage>
   );
 }
